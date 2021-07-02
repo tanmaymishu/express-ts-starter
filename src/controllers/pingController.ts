c0cbace0d1bf7916a71eaf9ac793165b8bb43f70
Tanmay Mishu <tanmaymishu@gmail.com>
2021-07-02 17:43:15 +0600

    Remove db:config command and add start command

--- src/controllers/pingController.ts
+++ src/controllers/pingController.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
 import { body } from "express-validator";
 
-export const schema = [
-    body('ping', "Must be NuMeric").isNumeric(),
-];
+export const schema = [body("ping", "Must be NuMeric").isNumeric()];
 
 export function pong(req: Request, res: Response) {
-    res.json({message: 'pong'});
+  res.json({ message: "pong" });
 }
