5d0b2aba25c496eb84f69ed7f5791ad0acb82e74
Tanmay Mishu <tanmaymishu@gmail.com>
2022-06-22 13:37:49 +0600

    Add postinstall script

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -8,18 +8,30 @@ if (process.env.NODE_ENV == 'testing') {
   dotenv.config({ path: '../../.env' });
 }
 
+function getConnectionConfig() {
+  if (
+    process.env.NODE_ENV === 'testing' &&
+    process.env.DB_CLIENT === 'sqlite3'
+  ) {
+    return ':memory';
+  }
+
+  if (process.env.DB_CLIENT === 'sqlite3') {
+    return { filename: './database.sqlite' };
+  }
+
+  return {
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+  };
+}
+
 const knexConfig: Knex.Config = {
   client: process.env.DB_CLIENT,
-  connection:
-    process.env.NODE_ENV == 'testing'
-      ? ':memory:'
-      : {
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD
-      },
-  useNullAsDefault: process.env.NODE_ENV == 'testing',
+  connection: getConnectionConfig(),
+  useNullAsDefault: process.env.DB_CLIENT == 'sqlite3',
   pool: {
     min: 2,
     max: 10
